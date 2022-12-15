5c1ea7d7715d4cadc441bc1e35e23911a138b25b
Davoud Taghawi-Nejad <davoud.taghawi-nejad@whiteshieldpartners.com>
2022-12-15 15:47:53 +0400

    Renaame insert_time_data insert_data

--- dbSeed.py
+++ dbSeed.py
@@ -9,24 +9,22 @@ client = Elasticsearch(
     hosts="https://db9d4a6fa7cd4393bffab8572a4119c5.centralus.azure.elastic-cloud.com:443", http_auth=("quantum", "i6Q8sbWO%v1c"))
 
 
-def insert_time_data(index, data):
+def insert_data(index, data):
     bulkOperation = {"index": {"_index": index}}
-
     bulkItems = []
     for item in data:
         bulkItems.append(bulkOperation)
         bulkItems.append(item)
-
     client.bulk(body=bulkItems)
 
 
 def main():
     t = time()
     time_series = generate_time_serieses()
-    insert_time_data("chart-data", time_series)
+    insert_data("chart-data", time_series)
     print(f'time {time() - t}')
     dashboard_data = generate_dashboard_data()
-    insert_time_data("dashboard-data", dashboard_data)
+    insert_data("dashboard-data", dashboard_data)
 
 
 if __name__ == '__main__':
